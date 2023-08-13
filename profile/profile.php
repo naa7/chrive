7daddfe66ca37b1120ef068692c7ed04375d9826
naa-7 <nalawad000@citymail.cuny.edu>
2023-08-13 01:00:31 -0400

    minor tweaks and improvements

--- profile/profile.php
+++ profile/profile.php
@@ -18,9 +18,36 @@ if (file_exists($envFilePath)) {
     die('.env file not found');
 }
 
-$uname = $_SESSION['username'];
+$message = "";
 $user_id = $_SESSION['user_id'];
+
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+
+    $stmt = $conn->prepare("UPDATE user SET name = ?, username = ?, email = ? WHERE id = ?");
+    $stmt->bind_param("sssi", $name, $username, $email, $user_id);
+
+    if ($stmt->execute()) {
+        $_SESSION['username'] = $username;
+    }
+
+    $stmt->close();
+
+    if (!empty($_POST['new_password']) && $_POST['new_password'] === $_POST['confirm_password']) {
+        $new_password = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+        $update_password_stmt = $conn->prepare("UPDATE user SET password = ? WHERE id = ?");
+        $update_password_stmt->bind_param("si", $new_password, $user_id);
+        $update_password_stmt->execute();
+        $update_password_stmt->close();
+    } else {
+        $message = "Passwords do not match";
+    }
+}
+
 $stmt = $conn->prepare("SELECT * FROM user WHERE id = ?");
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
@@ -70,30 +97,66 @@ $conn->close();
         <div class="profile-body">
             <div class="user-info">
                 <p class="user-info-header">User Profile</p>
-                <table class="info-table">
-                    <tr>
-                        <td class="info-label">Name:</td>
-                        <td class="info-value">
-                            <?php echo htmlspecialchars($row['name']); ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="info-label">Username:</td>
-                        <td class="info-value">
-                            <?php echo htmlspecialchars($row['username']); ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="info-label">Email:</td>
-                        <td class="info-value">
-                            <?php echo htmlspecialchars($row['email']); ?>
-                        </td>
-                    </tr>
-                </table>
+                <form action="" method="post">
+                    <table class="info-table">
+                        <tr>
+                            <td class="info-label">Name:</td>
+                            <td class="info-value">
+                                <?php if (!isset($_POST['edit'])): ?>
+                                    <span>
+                                        <?php echo htmlspecialchars($row['name']); ?>
+                                    </span>
+                                <?php else: ?>
+                                    <input type="text" name="name" value="<?php echo htmlspecialchars($row['name']); ?>">
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="info-label">Username:</td>
+                            <td class="info-value">
+                                <?php if (!isset($_POST['edit'])): ?>
+                                    <span>
+                                        <?php echo htmlspecialchars($row['username']); ?>
+                                    </span>
+                                <?php else: ?>
+                                    <input type="text" name="username"
+                                        value="<?php echo htmlspecialchars($row['username']); ?>">
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="info-label">Email:</td>
+                            <td class="info-value">
+                                <?php if (!isset($_POST['edit'])): ?>
+                                    <span>
+                                        <?php echo htmlspecialchars($row['email']); ?>
+                                    </span>
+                                <?php else: ?>
+                                    <input type="email" name="email" value="<?php echo htmlspecialchars($row['email']); ?>">
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <?php if (isset($_POST['edit'])): ?>
+                                <td class="info-label">Password:</td>
+                                <td class="info-value">
+                                    <input type="password" name="new_password" placeholder="New Password">
+                                    <input type="password" name="confirm_password" placeholder="Confirm Password">
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    </table>
+                    <p class="login-error">
+                        <?php echo $message; ?>
+                    </p>
+                    <?php if (!isset($_POST['edit'])): ?>
+                        <button type="submit" name="edit">Edit</button>
+                    <?php else: ?>
+                        <button type="submit" name="save">Save</button>
+                    <?php endif; ?>
+                </form>
             </div>
         </div>
-    </div>
-
     </div>
 </body>
 
